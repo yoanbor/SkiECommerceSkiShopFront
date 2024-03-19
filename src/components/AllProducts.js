defd1a542cd073bac5c2462280a1274fb5ff475e
Ylorklan <yoanbor@icloud.com>
2024-03-19 14:41:19 +0100

    refactor:() Changements de structure pour un code + propre

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
-import Cards from "../components/Cards";
+import Cards from "./Cards";
 
 
 const AllProducts = () => {
